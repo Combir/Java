ee525ca232d865e89d34315d5ce2055a146ccff3
Combir <144319259+Combir@users.noreply.github.com>
2024-09-30 11:08:00 +0300

    Update Problem7.java

--- Lection 2/Problem7.java
+++ Lection 2/Problem7.java
@@ -1,37 +1,48 @@
 import java.util.Scanner;
 
-public class Problem7 {
+class Problem7 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите количество поездок: ");
+        int n = in.nextInt();
 
-        // Input: number of trips planned
-        int n = scanner.nextInt();
-
-        // Ticket costs and number of trips per ticket
-        int[] costs = {15, 70, 125, 230, 440};
-        int[] trips = {1, 5, 10, 20, 60};
-
-        // Array to store the number of tickets for each type
-        int[] result = new int[5];
-
-        // Start with the largest ticket (60-trip), and work our way down
-        for (int i = 4; i >= 0; i--) {
-            // Determine how many tickets of this type we can use
-            result[i] = n / trips[i];
-            n %= trips[i];
-
-            // If there are remaining trips, we might need one more ticket of this type
-            if (n > 0 && i == 0) {
-                result[i]++;
-            }
+        // Проверка на отрицательное число
+        if (n < 0) {
+            System.out.println("Ошибка: количество поездок не может быть отрицательным.");
+            return; 
         }
 
-        // Print the results (number of tickets for each type)
-        for (int i = 0; i < 5; i++) {
-            System.out.print(result[i] + " ");
+        int ticket_60, ticket_20, ticket_10, ticket_5, ticket_1;
+
+        ticket_60 = n / 60;
+        n -= ticket_60 * 60;
+        if (n >= 36) {  // билет на 60 поездок
+            ticket_60++;
+            n = 0;
         }
 
-        // Close the scanner
-        scanner.close();
+        ticket_20 = n / 20;
+        n -= ticket_20 * 20;
+        if (n >= 18) {  // билет на 20 поездок
+            ticket_20++;
+            n = 0;
+        }
+
+        ticket_10 = n / 10;
+        n -= ticket_10 * 10;
+        if (n >= 9) {  // билет на 10 поездок
+            ticket_10++;
+            n = 0;
+        }
+
+        ticket_5 = n / 5;
+        n -= ticket_5 * 5;
+        ticket_1 = n % 5;
+  
+        System.out.printf("Ticket 1: %d \nTicket 5: %d \nTicket 10: %d \nTicket 20: %d \nTicket 60: %d\n",
+                ticket_1, ticket_5, ticket_10, ticket_20, ticket_60);
+
+        int price = ticket_1 * 15 + ticket_5 * 70 + ticket_10 * 125 + ticket_20 * 230 + ticket_60 * 440;
+        System.out.printf("Total price: %d\n", price);
     }
 }
